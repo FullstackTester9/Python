5c4fd0968318287f56cc9b40c8818e8af46dd4f1
Fullstack Tester <fullstack_tester@zohomail.in>
2026-07-15 19:56:10 +0530

    Arithmetic operators.

---
+++ Operators/Arithmetic_Operators.py
@@ -0,0 +1,47 @@
+"""
+This program demonstrates implementation of arithmetic operators.
+Following arithmetic operators are implemented: +, -, *, /, %, **, //
+"""
+
+# Addition
+num_1 = 12
+num_2 = 16.8
+result = num_1 + num_2
+print(f"Addition of {num_1} and {num_2} is: ", result, end='\n\n')
+
+# Subtraction
+num_1 = 91
+num_2 = 36
+result = num_1 - num_2
+print(f"Subtraction of {num_1} and {num_2} is: ", result, end='\n\n')
+
+# Multiplication
+num_1 = 12
+num_2 = 9
+result = num_1 * num_2
+print(f"Multiplication of {num_1} and {num_2} is: ", result, end='\n\n')
+
+# Division
+num_1 = 124
+num_2 = 3
+result = num_1 / num_2
+print(f"Division of {num_1} and {num_2} is: ", result, end='\n\n')
+
+# Modulus
+num_1 = 27
+num_2 = 4
+result = num_1 % num_2
+print(f"Modulus of {num_1} and {num_2} is: ", result, end='\n\n')
+
+# Exponentiation
+num_1 = 35
+num_2 = 2
+result = num_1 ** num_2
+print(f"Exponentiation of {num_1} and {num_2} is: ", result, end='\n\n')
+
+# Floor division
+num_1 = 20
+num_2 = 7
+result = num_1 // num_2
+print(f"Floor division of {num_1} and {num_2} is: ", result, end='\n\n')
+
