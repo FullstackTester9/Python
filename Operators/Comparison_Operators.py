d9cb43f460ab47bf23a5ba40e0f2b85dc4ee9c93
Fullstack Tester <fullstack_tester@zohomail.in>
2026-07-16 13:20:59 +0530

    Comparison operators completed.

---
+++ Operators/Comparison_Operators.py
@@ -0,0 +1,53 @@
+"""
+
+"""
+
+# '==' Equality operator.
+# Compares the values of two objects are equal or not.
+# Return result in boolean.
+
+print("'==' Equality operator")
+print("Is 10 equals to 10?", 10 == 10)
+print("Is 5 equals to 2?", 5 == 2)
+
+list_a = [1, 2, 3]
+list_b = [1, 2, 3]
+print(f"{list_a} == {list_b}: ", list_a == list_b, end='\n\n')
+
+# '!=' Not equal to operator
+# Compares the values of two objects and return boolean value.
+
+print("'!=' Not equal to operator")
+print("15 != 8: ", 15 != 8)
+print("5 != 5: ", 5 != 5, end='\n\n')
+
+# '>' Greater than operator.
+# Compare two values and return true if value is greater.
+
+print("'>' Greater than operator")
+print("7 > 5: ", 7 > 5)
+print("3 > 5: ", 3 > 5)
+print("7 > 7: ", 7 > 7, end='\n\n')
+
+# '<' Less than operator.
+# Compare two values and return true if value is lesser.
+
+print("'<' Less than operator.")
+print("7 < 5: ", 7 < 5)
+print("3 < 5: ", 3 < 5)
+print("7 < 7: ", 7 < 7, end='\n\n')
+
+# '>=' Greater than or equal to operator.
+# Compare two values and return true if value is greater or equal.
+
+print("'>=' Greater than or equal to operator")
+print("10 >= 10: ", 10 >= 10)
+print("10 >= 9: ", 10 >= 9)
+print("10 >= 11: ", 10 >= 11, end='\n\n')
+
+ # '<=' Less than or equal to operator.
+# Compare two values and return true if value is less or equal.
+
+print("'<=' Less than or equal to operator")
+print("5 <= 5: ", 5 <= 5)
+print("5 <= 3: ", 5 <= 3)
